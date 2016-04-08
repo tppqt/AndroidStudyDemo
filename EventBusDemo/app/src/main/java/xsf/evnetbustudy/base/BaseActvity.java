3998835a37a9485cfc5c03336c909aad3df55b91
uestc_xsf <xsf_uestc_ncl@163.com>
2016-04-08 23:54:06 +0800

    test super class @Subscribe

--- EventBusDemo/app/src/main/java/xsf/evnetbustudy/base/BaseActvity.java
+++ EventBusDemo/app/src/main/java/xsf/evnetbustudy/base/BaseActvity.java
@@ -2,9 +2,15 @@ package xsf.evnetbustudy.base;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.io.Serializable;
+
+import xsf.evnetbustudy.event.TestEvent;
 
 /**
  * @author xushangfei
@@ -13,6 +19,7 @@ import java.io.Serializable;
  */
 public class BaseActvity extends AppCompatActivity implements View.OnClickListener {
     public static final String SER_KEY = "object";
+
 
     public void launch(Class<?> clazz) {
         Intent intent = new Intent(this, clazz);
@@ -23,6 +30,14 @@ public class BaseActvity extends AppCompatActivity implements View.OnClickListen
         Intent intent = new Intent(this, clazz);
         intent.putExtra(SER_KEY, (Serializable) object);
         startActivity(intent);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void doMain2(TestEvent event) {
+        String msg = "onEventMainThread get the msg: \n" + event.getMsg();
+        // tv_msg.setText(msg);
+        Log.d("BaseActvity thread main", Thread.currentThread().getName());
+        //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
 
