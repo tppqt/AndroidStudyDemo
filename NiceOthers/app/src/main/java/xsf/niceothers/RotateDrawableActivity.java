fd627f77293eb7e7c3a22a5a92f1ed0b4b7ce4e9
uestc_xsf <xsf_uestc_ncl@163.com>
2016-06-10 15:56:29 +0800

    selector等初步工作

--- NiceOthers/app/src/main/java/xsf/niceothers/RotateDrawableActivity.java
+++ NiceOthers/app/src/main/java/xsf/niceothers/RotateDrawableActivity.java
@@ -1,12 +1,26 @@
 package xsf.niceothers;
 
+import android.media.MediaPlayer;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import xsf.niceothers.base.BaseActvity;
 
 public class RotateDrawableActivity extends BaseActvity {
-    private TextView tvShow;
+    private final int Duration = 600;  // 动画时长
+
+    private AnimatorState state = AnimatorState.State_Stop;  //动画状态
+    private AudioState audioState = AudioState.STATE_STOP;   //音乐播放器状态
+
+    private ImageView btnPre, btnPlay, btnNext;
+    private ImageView cdBox, handerd;
+
+    private MediaPlayer mediaPlayer;
+
+    private boolean flag = false;  //标记，控制唱片旋转
+
+    private String names[] = {"demo01.mp3", "demo02.mp3"};
+    private int position = 0;
 
 
     @Override
@@ -16,21 +30,40 @@ public class RotateDrawableActivity extends BaseActvity {
 
     @Override
     protected void initView() {
-        getSupportActionBar().setTitle("MVC");
-        tvShow = IfindViewById(R.id.tvShowInfo);
-        IfindViewById(R.id.btn_getInfo).setOnClickListener(this);
+        getSupportActionBar().setTitle("RotateDrawble");
+        cdBox = IfindViewById(android.R.id.progress);
+        handerd = IfindViewById(android.R.id.background);
+
+        btnPre = IfindViewById(android.R.id.button1);
+        btnPlay = IfindViewById(android.R.id.button2);
+        btnNext = IfindViewById(android.R.id.button3);
+        btnPre.setOnClickListener(this);
+        btnPlay.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_getInfo:
-                showWeatherInfo();
+
                 break;
         }
     }
 
-    private void showWeatherInfo() {
+    //----------------------------------功能函数----------------------------------
 
+    public enum AnimatorState {
+        State_Stop,
+        State_Playing
     }
+
+    public enum AudioState {
+        STATE_STOP,
+        STATE_PAUSE,
+        STATE_PREPARE,
+        STATE_PLAYING
+    }
+
 }
